9ff9429f038e94351d33f44f85932c1dda6fb290
Wthrud05 <alex.electrik220@gmail.com>
2022-09-08 13:16:59 +0300

    fixing

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navigtation.module.scss';
 
-const links = [
-  { name: 'Comments', to: '/' },
-  { name: 'Posts', to: '/posts' },
-];
-
-const isActive = ({ isActive }) => (isActive ? styles.Activelink : '');
-
 const Navigation = ({ getCategory }) => {
+  const links = [
+    { name: 'Comments', to: '/react-search' },
+    { name: 'Posts', to: '/posts' },
+  ];
+
+  const isActive = ({ isActive }) => (isActive ? styles.Activelink : '');
+
   return (
     <ul className={styles.Navigation}>
       {links.map((link) => (
-        <li className={styles.item} key={link.name}>
+        <li key={link.name}>
           <NavLink
-            onClick={() => getCategory(link.name.toLowerCase())}
+            name={link.name}
             className={isActive}
-            to={link.to}>
+            to={link.to}
+            onClick={() => getCategory(link.name.toLowerCase())}>
             {link.name}
           </NavLink>
         </li>
